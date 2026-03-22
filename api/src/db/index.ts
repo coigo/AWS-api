881e61daec5ff321788940b38e8fea93196ef118
Miguel <migueelcoi@gmail.com>
2026-03-22 12:26:36 -0400

    corrigindo drizzle env

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -2,9 +2,17 @@ import 'dotenv/config';
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
   
-console.log(process.env["DATABASE_URL"])
+console.log(`
+    host: ${process.env.DB_HOST!}
+    user: ${process.env.DB_USER!}
+    password: ${process.env.DB_PASSWORD!}
+    database: ${process.env.DB_NAME!}`
+)
 
 const poolConnection = mysql.createPool({
-    uri:process.env["DATABASE_URL"]
+    host: process.env.DB_HOST!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
 });
 export const db = drizzle({ client: poolConnection });
