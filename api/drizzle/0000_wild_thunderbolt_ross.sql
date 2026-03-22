21c7cabfbeb5713f4e1fa2313e3d69475f4f20d3
Miguel <migueelcoi@gmail.com>
2026-03-22 11:24:40 -0400

    weee

---
+++ api/drizzle/0000_wild_thunderbolt_ross.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users_table` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	CONSTRAINT `users_table_id` PRIMARY KEY(`id`)
+);
---
+++ api/src/db/schema.ts
@@ -0,0 +1,6 @@
+import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core';
+
+export const usersTable = mysqlTable('users_table', {
+  id: serial().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+});
